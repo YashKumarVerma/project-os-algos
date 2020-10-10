5c4ecf850d6e93d1b88cc006786de51294cade79
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-10-10 14:33:11 +0530

    Read logs and parse entries

---
+++ includes/log_processor/log_processor.h
@@ -0,0 +1,26 @@
+#ifndef log_processor
+#define log_processor
+
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class LogEntry{
+    public:
+        string commandType;
+        string command;
+        unsigned long long int timestamp;
+        
+        LogEntry(string commandType, string command, string timestamp);
+};
+
+class LogProcessor{
+    private: 
+        vector<string> tokenizeEntry(string entry);
+    public:
+        vector<LogEntry> entryStorage;
+        LogProcessor(vector<string> entries);
+};
+
+#endif
--- source/app.cpp
+++ source/app.cpp
@@ -1,19 +1,35 @@
 #include<iostream>
 #include"./../includes/rainbow/rainbow.h"
 #include"./../includes/logger/logger.cpp"
+#include"./../includes/watcher/watcher.h"
+#include"./../includes/log_processor/log_processor.h"
+
 using namespace std;
 
 int main(){
     cout << rainbow::underline(rainbow::bold(rainbow::red("SELF HEALING OS"))) << endl << endl; 
     
-    cout << rainbow::bold("Logger Usage") << endl;
-    
+    cout << rainbow::bold("Logger Usage") << endl << endl;
+
     LoggerModule logger;
     logger.save("system", "ls");
     logger.save("system", "mkdir directory");
     logger.save("system", "cat commands.txt");
+    logger.save("system", "ls -a ");
     logger.save("internal", ":checkpoint create");
-    logger.save("internal", ":checkpoint delete");
-      
+    logger.save("system", "tree .");
+    logger.save("system", "vim task.md");
+    logger.save("system", "rm *.txt");
+    logger.save("internal", ":checkpoint create");
+
+
+    cout << endl << rainbow::bold(rainbow::underline("Reading and Processing Logs")) << endl;
+    vector<string> commands = logger.readAll();
+
+    LogProcessor logProcessor(commands);
+    for(auto iter : logProcessor.entryStorage){
+        cout << iter.timestamp << endl;
+    }
+
     return 0;
 }
